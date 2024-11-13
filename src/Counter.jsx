c27fb3989bf09e32c8dfe79627af81ca9a8cdc55
Faheema_Fasil <faheemafasil12@gmail.com>
2024-11-13 12:37:06 +0530

    changes

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { decrement, increment, reset } from './Redux/Slice/CounterSlice'
 
 function Counter() {
   const count = useSelector((state) =>state.counterReducer.count)
   const dispatch=useDispatch()
+  const[amount,setAmount]=useState("")
+  console.log(amount);
+  
   return (
     <>
 
@@ -17,8 +20,8 @@ function Counter() {
             <button className='btn btn-warning' onClick={()=>dispatch(decrement())}>Decrement</button>
         </div>
         <div className="d-flex mt-5">
-            <input type="text" className='form-control' placeholder='enter amount to be incremented'/>
-            <button className='btn btn-primary ms-3'>Increment Aomunt</button>
+            <input type="text" className='form-control' onChange={e=>setAmount(e.target.value)} placeholder='enter amount to be incremented'/>
+            <button className='btn btn-primary ms-3' >Increment Aomunt</button>
         </div>
         
     </div>
